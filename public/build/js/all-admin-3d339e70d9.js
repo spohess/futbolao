593e5676279b78e756db12de2b6a535b6c75de8e
Sergio Hess <hess.sergio@gmail.com>
2016-11-19 20:39:18 -0200

    Atualizando a estrutura de desenvolvimento

--- public/build/js/all-admin-3d339e70d9.js
+++
@@ -1,1 +0,0 @@
-function fnSistemaController(o,n){o.onLoadAdmin=function(){o.carregaUsuarios()},o.carregaUsuarios=function(){n.get("/admin/usuario/lista_usuarios").success(function(n){o.listaUsuarios=n})}}fnSistemaController.$inject=["$scope","$http"],angular.module("app").controller("sistemaController",fnSistemaController);
