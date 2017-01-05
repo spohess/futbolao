30fadef3042b6c7f7f70e7c968ec3e031c6e303d
Sergio Hess <hess.sergio@gmail.com>
2017-01-04 23:55:10 -0200

    minify

--- public/build/js/all-palpite-9725ced845.js
+++
@@ -1,6 +0,0 @@
-function fnSistemaController($scope, $http) {
-
-}
-fnSistemaController.$inject = ['$scope', '$http'];
-angular.module('app').controller('sistemaController', fnSistemaController);
-//# sourceMappingURL=all-palpite.js.map
